2da787d6cb5ae5368e38a04e5cb630b3d205aef6
frnksinatra <ferasbaraka0@gmail.com>
2017-06-27 18:25:44 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * CHECKLIST
+ * Need to define my variables
+ * Create the prompts for the user input
+ * Use the If/Else statements to make use of the input
+ *     Use the modular(%) to divide numbers to check whether it is even or not.
+ *     Use tied boolean conditionals to validate input
+ * Ask the user if they want to terminate the program
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Define them variables son.
+        
+    }
+}
