e27362f504876e41ccd86a135106ddca69e89e1f
frnksinatra <ferasbaraka0@gmail.com>
2017-06-27 23:52:37 -0400

    added code to collect userName

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
  * Need to define my variables
  * Create the prompts for the user input
  * Use the If/Else statements to make use of the input
- *     Use the modular(%) to divide numbers to check whether it is even or not.
- *     Use tied boolean conditionals to validate input
+ * Use the modular(%) to divide numbers to check whether it is even or not.
+ * Use tied boolean conditionals to validate input
  * Ask the user if they want to terminate the program
- *
  */
 
 
@@ -16,6 +15,23 @@ public class Main {
 
     public static void main(String[] args) {
         //Define them variables son.
-        
+
+        String cont = "y"; //defines my program loop
+        String userName;
+        int userInput;
+        int userInput2;
+        int userInput3;
+
+
+        //Creating the prompts
+        System.out.println("What is your name?");
+
+        //Setting up the scanner called input
+        Scanner input = new Scanner(System.in);
+
+        userName = (input.next());
+
+        System.out.println("Thank you, " + userName);
+
     }
 }
